635bc3b62102c644523048dc526bef8f4fcb0ca3
garciaEstefania <garcia_estefannia@hotmail.com>
2021-07-24 06:04:36 -0600

    Add component to display pokemon info

--- app/components/PokeCard.js
+++ app/components/PokeCard.js
@@ -1,71 +1,67 @@
 import React, {useState} from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 import styles from "../assets/styles/style";
-// import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const PokeCard = ({pokemon, selectedValues, setSelectedValues, isViewing}) => {
-    
-const foundPokemon = selectedValues?.find((pok) => pok.id === pokemon.id);
-console.log('selectedValues: ',foundPokemon);
+const PokeCard = ({pokemon, pokeTeam, handleTeam: setTeam, isViewing}) => {
+const foundPokemon = pokeTeam?.find(p => p.id === pokemon.id);
 const [selectedPokemon, setSelectedPokemon] = useState(foundPokemon);
 
-    const onPressedCard = () => {
-        if (!selectedPokemon) {
-            
-            if (selectedValues.length < 6) {
-                setSelectedPokemon(pokemon);
-                setSelectedValues((prevState) => prevState.concat(pokemon));
-            } else {
-            return;
-            }
+    const handleTeam = () => {
+      if(!selectedPokemon){
+        if(pokeTeam.length < 6) {
+          setSelectedPokemon(pokemon);
+          setTeam(pokemon, 'add');
         } else {
-            setSelectedPokemon(null);
-            setSelectedValues((prevState) =>
-                prevState.filter((det) => det.id !== pokemon.id),
-            );
+          return;
         }
-    };
+      } else {
+        setTeam(pokemon, 'remove');
+        setSelectedPokemon(null);
+      }
+  }
 
   return (
-    <View style={[styles.cardItem, { backgroundColor: !selectedPokemon ? '#fff' : 'rgba(0,0,0,0.8)', elevation: !selectedPokemon ? 5 : 0, },]}>
+    <View style={[styles.cardItem, { backgroundColor: !selectedPokemon  ? '#fff'   : 'rgba(0,0,0,0.2)' , elevation: !selectedPokemon  ? 5 : 0, },]}>
       {!isViewing ? (
         <TouchableOpacity
         style={styles.btnContainer}
-        onPress={onPressedCard}
+        onPress={handleTeam}
         >
-            <View style={styles.cardContainer}>
-                <View style={styles.imageContainer}>
-                {!pokemon.image && pokemon.image !== null ? (
-                    <ActivityIndicator color="red" size="small" />
-                  ) : (
-                    <Image
-                      style={styles.pokeImage}
-                      source={
-                        pokemon.image !== null
-                          ? {
-                              uri: pokemon.image,
-                            }
-                          : require('../assets/img/noImageAvailable.png')
-                      }
-                    />
-                  )}
-                </View>
-                <View style={styles.infoContainer}>
-                    <Text># {pokemon.id}</Text>
-                    <Text style={styles.pokeName}>{pokemon.name}</Text>
-                    <Text>{pokemon.description}</Text>
-                    <View style={styles.textRow}>
-                        <Text>Type: </Text>
-                        {pokemon.types?.map((type, index) => (
-                        <Text key={index}>
-                            {type.type.name}
-                            {pokemon.types?.length > 1 && index === 0 && (
-                            <Text> / </Text>
-                            )}
-                        </Text>
-                        ))}
-                    </View>
-                </View>
+            <View>
+              <View style={styles.cardContainer}>
+                  <View style={styles.imageContainer}>
+                  {!pokemon.image && pokemon.image !== null ? (
+                      <ActivityIndicator color="red" size="small" />
+                    ) : (
+                      <Image
+                        style={styles.pokeImage}
+                        source={
+                          pokemon.image !== null
+                            ? {
+                                uri: pokemon.image,
+                              }
+                            : require('../assets/img/noImageAvailable.png')
+                        }
+                      />
+                    )}
+                  </View>
+                  <View style={styles.infoContainer}>
+                      <Text># {pokemon.id}</Text>
+                      <Text style={styles.pokeName}>{pokemon.name}</Text>
+                      <Text style={styles.pokeDesc}>{pokemon.description}</Text>
+                      <View style={styles.textRow}>
+                          <Text>Type: </Text>
+                          {pokemon.types?.map((type, index) => (
+                          <Text key={index}>
+                              {type.type.name}
+                              {pokemon.types?.length > 1 && index === 0 && (
+                              <Text> / </Text>
+                              )}
+                          </Text>
+                          ))}
+                      </View>
+                  </View>
+              </View>
             </View>
         </TouchableOpacity>
         ) : (
